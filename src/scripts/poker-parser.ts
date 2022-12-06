cecb669fb766d2faf4381916688e96fa4454e0e3
“Austin <onsclome@onsclom.net>
2022-12-05 20:45:23 -0800

    Make prototype

---
+++ src/scripts/poker-parser.ts
@@ -0,0 +1,52 @@
+export function parseNight(
+  csv: string,
+  bankerName: string,
+  bankerBuyin: number,
+) {
+  const transactions = csv
+    .split("\n")
+    .map((line) => {
+      const [, id, datetime, type, status, note, from, to, amount] = line.split(
+        ",",
+      );
+      return { id, datetime, type, status, note, from, to, amount };
+    });
+
+  return [...new Set(transactions.map((t) => t.from))].map((p) => {
+    const buy = transactions.filter((t) => t.from == p)
+      .map((t) => t.amount)
+      .map((a) => Number(a!.split("$")[1]))
+      .reduce((a, b) => a + b, 0);
+
+    const cashout = transactions.filter((t) => t.to == p)
+      .map((t) => t.amount)
+      .map((a) => Number(a!.split("$")[1]))
+      .reduce((a, b) => a + b, 0);
+
+    if (p == bankerName) {
+      const totalBuyin = transactions.filter((t) => t.from != bankerName)
+        .map((t) => t.amount).map((a) => Number(a!.split("$")[1])).reduce(
+          (a, b) => a + b,
+          0,
+        );
+      const totalCashout = transactions.filter((t) => t.from == bankerName)
+        .map((t) => t.amount).map((a) => Number(a!.split("$")[1])).reduce(
+          (a, b) => a + b,
+          0,
+        );
+      return {
+        name: p,
+        buy: bankerName,
+        cashout: Math.max(0, bankerBuyin + totalBuyin - totalCashout),
+        net: totalBuyin - totalCashout,
+      };
+    }
+
+    return {
+      name: p,
+      buyin: buy,
+      cashout,
+      net: cashout - buy,
+    };
+  });
+}
